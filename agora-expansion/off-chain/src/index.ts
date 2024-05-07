8e7a9c34ee0c0201057a87c3dd5bf08d065fedb1
Riley-Kilgore <Riley_Kilgore@outlook.com>
2024-05-06 23:56:29 -0700

    Added tests and some fixes, off-chain to be addressed shortly

--- agora-expansion/off-chain/src/index.ts
+++ agora-expansion/off-chain/src/index.ts
@@ -2,6 +2,8 @@ import { Assets } from 'lucid-cardano';
 import {GovernanceTokenLocker, GovernanceTokenMinter} from '../plutus'
 
 type ConfigParameter = { action: "Sum" | "Multiply"; powerList: { weight: bigint; assetConfig: { AssetSelection: { assetSelection: { PolicyId: { policy: string; }; } | { AssetClass: { policy: string; asset: string; }; } | { ...; }; }; } | { ...; }; }[]; }
+// We have to finish writing the parameter bullshit.
+
 
 export const getLocker = async (param: ConfigParameter) => {
     return new GovernanceTokenLocker(param)
@@ -25,3 +27,7 @@ export const retrieveTokens = async (receipt: string) => {
 }
 
 // TODO - Also need to write an emulator test which demonstrates the functionality of the system.
+
+// Today we need to address the remaining code on this, we should be able to complete it before dinner time and in that case we could spend a lot of time tomorrow on other things.
+
+// If we cannot finish this today it will bleed into this coming week which is not great. I think it would be optimal to finish it today, I want to drink some water, I want to eat some food, but I can eat food later, I can drink water now. I can take a break in roughly 105 minutes.
